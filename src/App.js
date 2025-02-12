2bac1c3eb5ed425cb12191b866d3f2c20ef81c27
allatsv <cvetkova20022@gmail.com>
2025-02-12 19:30:30 +0300

    saruarStart

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Services from './components/Services';
+function App() {
+  return (
+    <div className="App">
+      <Services/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Services.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+
+
+function Services(){
+    return(
+        <section className="services">
+            <div className="container">
+                <div className="services__inner">
+                    <h3 className="subtitle services__subtitle">Saruar Logistics</h3>
+                    <h2 className="title services__title">Наши услуги</h2>
+                    <p className="services__description">Эффективные логистические решения для вашего бизнеса</p>
+                </div>
+                <div className="services__boxes">
+                <div className="box">
+                        <div className="box__img">
+                            <img src="../../616b6baefa48322ddcf6cd99b1f7e7ab.png" alt="" />
+                        </div>
+                        <div className="box__inner">
+                            <h3 className="box__title">Автомобильные перевозки</h3>
+                            <a className="box__link" href="">
+
+                            <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="14.5" cy="14.5" r="14.5" fill="white"/>
+<path d="M21.7794 15.0127C22.0625 14.7295 22.0625 14.2705 21.7794 13.9873L17.1655 9.37348C16.8824 9.09035 16.4233 9.09035 16.1402 9.37348C15.8571 9.65661 15.8571 10.1156 16.1402 10.3988L20.2414 14.5L16.1402 18.6012C15.8571 18.8843 15.8571 19.3434 16.1402 19.6265C16.4233 19.9097 16.8824 19.9097 17.1655 19.6265L21.7794 15.0127ZM7.7334 15.225L21.2667 15.225L21.2667 13.775L7.7334 13.775L7.7334 15.225Z" fill="#CC1C19"/>
+</svg>
+
+</a>
+                        </div>
+                        <ul className="box__list">
+                            <li className="box__item">
+                            FTL (Full Truck Load).
+                            </li>
+                            <li className="box__item">
+                            Перевозки рефрижераторами.
+                            </li>
+                            <li className="box__item">
+                            Перевозка негабаритных грузов.
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="box">
+                        <div className="box__img">
+                        <img src="../../d121b1620cc8cde5a38ca51d8e5be347.jpg" alt="" />
+                           
+                        </div>
+                        <div className="box__inner">
+                            <h3 className="box__title">Мультимодальные
+                            перевозки</h3>
+                            <a className="box__link" href="">
+
+                            <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="14.5" cy="14.5" r="14.5" fill="white"/>
+<path d="M21.7794 15.0127C22.0625 14.7295 22.0625 14.2705 21.7794 13.9873L17.1655 9.37348C16.8824 9.09035 16.4233 9.09035 16.1402 9.37348C15.8571 9.65661 15.8571 10.1156 16.1402 10.3988L20.2414 14.5L16.1402 18.6012C15.8571 18.8843 15.8571 19.3434 16.1402 19.6265C16.4233 19.9097 16.8824 19.9097 17.1655 19.6265L21.7794 15.0127ZM7.7334 15.225L21.2667 15.225L21.2667 13.775L7.7334 13.775L7.7334 15.225Z" fill="#CC1C19"/>
+</svg>
+
+</a>
+                        </div>
+                        <ul className="box__list">
+                            <li className="box__item">
+                            Сочетание автомильного и железнодорожного транспорта.
+                            </li>
+
+                        </ul>
+                    </div>
+                    <div className="box">
+                        <div className="box__img">
+                        <img src="../../d1a2273a7bfe87dc916543c117bfc6e3.jpg" alt="" />
+                        </div>
+                        <div className="box__inner">
+                            <h3 className="box__title">Перевозка опасных грузов</h3>
+                            <a className="box__link" href="">
+
+                            <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="14.5" cy="14.5" r="14.5" fill="white"/>
+<path d="M21.7794 15.0127C22.0625 14.7295 22.0625 14.2705 21.7794 13.9873L17.1655 9.37348C16.8824 9.09035 16.4233 9.09035 16.1402 9.37348C15.8571 9.65661 15.8571 10.1156 16.1402 10.3988L20.2414 14.5L16.1402 18.6012C15.8571 18.8843 15.8571 19.3434 16.1402 19.6265C16.4233 19.9097 16.8824 19.9097 17.1655 19.6265L21.7794 15.0127ZM7.7334 15.225L21.2667 15.225L21.2667 13.775L7.7334 13.775L7.7334 15.225Z" fill="#CC1C19"/>
+</svg>
+
+</a>
+                        </div>
+                        <ul className="box__list">
+                            <li className="box__item">
+                            Специфика, безопасность, опыт.
+                            </li>
+                        
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    )
+}
+
+export default Services
